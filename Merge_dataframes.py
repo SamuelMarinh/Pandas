e050e78d0a0ced17fefe287bace0ee8be44b91b4
SamuelMarinh <samuelmarinhodealmeida@gmail.com>
2023-05-11 15:00:37 -0300

    Up merge

---
+++ Merge_dataframes.py
@@ -0,0 +1,7 @@
+cadastro_a = {'Id': ['AA2930','BB4563','CC2139','DE2521','GT3462','HH1158'],
+              'Nome': ['Victor', 'Amanda', 'Bruna', 'Carlos', 'Ricardo', 'Maria'],
+              'Idade': [20,35,40,54,30,27],
+              'CEP':['00092-029','11111-111','22222-888','00000-999','88888-111','77777-666']}
+
+cadastro_a = pd.DataFrame(cadastro_a, columns = ['Id', 'Nome', 'Idade', 'CEP'])
+cadastro_a
